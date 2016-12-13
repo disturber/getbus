a38ed57d2751488db8541b90aa4b9baaee358cc6
disturber <disturber@users.noreply.github.com>
2016-12-13 14:59:25 +0200

    first getbus
    example

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "solodisturber@gmail.com";
+$sitename = "GetBus";
+
+$name = trim($_POST["name"]);
+$company = trim($_POST["company"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$message = "Имя: $name \nТелефон: $phone \nКомпания: $company \nПочта: $email";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
